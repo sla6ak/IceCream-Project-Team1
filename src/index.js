a5b38cf02a7c4592b150fdb39d44546bc2845be8
Leonid578 <Leonid.Ivanov187@gmail.com>
2021-10-09 20:15:26 +0300

    modal

--- src/index.js
+++ src/index.js
@@ -18,3 +18,24 @@ import './sass/main.scss';
       refs.modald.classList.toggle('scroll');
     }
   })();
+
+
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+(() => {
+  const refs = {
+    openModalBtn2: document.querySelector('[data-footermodal-open]'),
+    closeModalBtn2: document.querySelector('[data-footermodal-close]'),
+    modal2: document.querySelector('[data-footermodal]'),
+    modald2: document.querySelector('[data-locationd]'),
+  };
+
+  refs.openModalBtn2.addEventListener('click', toggleModal2);
+  refs.closeModalBtn2.addEventListener('click', toggleModal2);
+
+  function toggleModal2() {
+    refs.modal2.classList.toggle('is-hidden');
+    refs.modald2.classList.toggle('scroll');
+  }
+})();
