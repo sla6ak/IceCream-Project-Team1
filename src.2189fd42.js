3bf6c667803a381662f4fe6e0e229895344673d1
sla6ak <86052868+sla6ak@users.noreply.github.com>
2021-10-09 02:46:20 +0000

    Deploying to gh-pages from @ sla6ak/IceCream-Project-Team1@5a99195c1464f3492c44b759e21af05ef83d07bf 🚀

--- src.2189fd42.js
+++ src.2189fd42.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/IceCream-Project-Team1/src.59e44fc2.js.map
+//# sourceMappingURL=/IceCream-Project-Team1/src.2189fd42.js.map
