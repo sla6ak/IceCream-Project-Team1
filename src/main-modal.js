3d47ba2d58c127077fa67a0cba31195198cdb333
Viktor <86052868+sla6ak@users.noreply.github.com>
2021-10-12 02:50:23 +0300

    js for button moby menu

--- src/main-modal.js
+++ src/main-modal.js
@@ -1,11 +1,13 @@
 (() => {
     const refs = {
       openModalBtn: document.querySelector('[data-mainmodal-open]'),
+      openModalBtn2: document.querySelector('.buttonchic'),
       closeModalBtn: document.querySelector('[data-mainmodal-close]'),
       modal: document.querySelector('[data-mainmodal]'),
     };
   
     refs.openModalBtn.addEventListener('click', toggleMainmodal);
+    refs.openModalBtn2.addEventListener('click', toggleMainmodal);
     refs.closeModalBtn.addEventListener('click', toggleMainmodal);
   
     function toggleMainmodal() {
