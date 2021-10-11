085186678590ddc3b6161185fbf89643729219fa
SvetlanaLytovchenko <sveta141084@gmail.com>
2021-10-12 01:39:21 +0300

    модалка не подключается

---
+++ src/modal-about.js
@@ -0,0 +1,16 @@
+(() => {
+    const refss = {
+      openModalBtn3: document.querySelector('[data-aboutmodal-open]'),
+      closeModalBtn3: document.querySelector('[data-aboutmodal-close]'),
+      modal3: document.querySelector('[data-aboutmodal]'),
+      modald3: document.querySelector('[data-locationd]'),
+    };
+  
+    refss.openModalBtn3.addEventListener('click', toggleModal3);
+    refss.closeModalBtn3.addEventListener('click', toggleModal3);
+  
+    function toggleModal3() {
+      refss.modal3.classList.toggle('is-hidden');
+      refss.modald3.classList.toggle('scroll');
+    }
+  })();
