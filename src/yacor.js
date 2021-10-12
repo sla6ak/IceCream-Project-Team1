425550f30ca41937b27e14a6b1221638c2412370
Viktor <86052868+sla6ak@users.noreply.github.com>
2021-10-13 02:37:44 +0300

    jquery for a

---
+++ src/yacor.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $(".yacor").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+});
