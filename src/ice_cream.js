e1295b380048bf88a27392a2a34b61a16f8d6544
Romanna Yasinska <r.bren.1991@gmail.com>
2021-10-13 00:36:41 +0300

    Додала анімацію і якорні ссилки на секції

--- src/ice_cream.js
+++ src/ice_cream.js
@@ -1,9 +1,10 @@
-const ice = document.querySelector(".hero-counter__image")
-
+const ice = document.querySelector(".hero-info__picture")
+const girl = document.querySelector(".hero-counter__image")
 setTimeout(() =>{
     leto()
 },1000);
 
 function leto(){
-ice.classList.add("prili-leto")
+    ice.classList.add("prili-leto")
+    girl.classList.add("prili-girl")
 }
